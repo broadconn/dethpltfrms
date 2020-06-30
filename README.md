029520b5289e565490145b14f6f72bafde5d43d3
Connor <connor.broad@gmail.com>
2020-06-30 18:30:36 +1200

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# deth 
+
+Developed with Unreal Engine 4
---
+++ Source/DeathPlatforms/DeathPlatformsGameMode.h
@@ -0,0 +1,19 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "DeathPlatformsGameMode.generated.h"
+
+UCLASS(minimalapi)
+class ADeathPlatformsGameMode : public AGameModeBase
+{
+	GENERATED_BODY()
+
+public:
+	ADeathPlatformsGameMode();
+};
+
+
+
